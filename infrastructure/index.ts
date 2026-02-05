a01bd38afda9338a765afb1957f72bb1c263e5b4
Victor <glas0069@algonquinlive.com>
2026-02-05 16:06:54 -0500

    added for testing when pulumi runs

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -39,3 +39,6 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
+
+export const acrServer = registry.loginServer
+export const acrUsername = registryCredentials.username
