e5c8c8b666f677411000ffaa28a9d8fde5f04186
VictorGlasser <glas0069@algonquinlive.com>
2026-02-05 23:17:57 -0500

    created a managed Redis service

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,6 +3,7 @@ import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as dockerBuild from '@pulumi/docker-build'
 import * as containerinstance from '@pulumi/azure-native/containerinstance'
+import * as cache from '@pulumi/azure-native/cache'
 
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config()
@@ -19,6 +20,24 @@ const memory = config.requireNumber('memory')
 
 // Create a resource group.
 const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+
+// Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'westus3',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru'
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0
+  }
+})
 
 // Create the container registry.
 const registry = new containerregistry.Registry(`${prefixName}ACR`, {
